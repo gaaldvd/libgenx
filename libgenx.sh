1e08dd5ea26d4008020bdf6dc15fbcefbdf1df61
gaaldvd <gaaldvd@proton.me>
2024-06-30 21:19:47 +0200

    start script  update
    now it echoes usage if no args are given

--- libgenx.sh
+++ libgenx.sh
@@ -1,3 +1,9 @@
+if [ "$#" -eq 0 ]
+  then
+    echo "> Usage: $0 [-c/-g] [-a author] [-t title]"
+    exit 1
+fi
+
 while getopts "cga:t:" flag; do
   case $flag in
     c) mode="cli";;
@@ -9,6 +15,8 @@ while getopts "cga:t:" flag; do
       exit 1;;
   esac
 done
+
+
 
 echo "> Welcome!"
 
