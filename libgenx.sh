06a7dd8eb265a7990d57fa08823dad683fea73ce
gaaldvd <gaaldvd@proton.me>
2024-12-06 18:46:43 +0100

    main loop established

--- libgenx.sh
+++ libgenx.sh
@@ -1,12 +1,16 @@
 # validate options
 if [ "$#" -eq 0 ]; then
-    echo "> Usage: $0 [-c/-g] ['query']"
+    echo "> Usage: $0 [-u] [-c/-g] ['query']"
     exit 1
 fi
 
 # interpret options
-while getopts "cg" flag; do
+while getopts "ucg" flag; do
   case $flag in
+    u)
+      echo "> Updating LibGenX..."
+      ./update.sh
+      exit 0;;
     c) mode="cli";;
     g) mode="gui";;
     *)
@@ -15,12 +19,11 @@ while getopts "cg" flag; do
   esac
 done
 
+# start application
 if [ -n "$2" ]; then
-    echo "> Query: $2"
+    # echo "> Query: $2"
+    pipenv run python src/libgenx_$mode.py "$2"
 else
-    echo "> No query specified."
+    # echo "> No query specified."
+    pipenv run python src/libgenx_$mode.py
 fi
-
-# start script
-# clear
-pipenv run python src/libgenx_$mode.py "$2"
--- src/libgenx_cli.py
+++ src/libgenx_cli.py
@@ -1,6 +1,47 @@
-"""LibGenX CLI script.
+"""
+LibGenX CLI script.
 """
 
 import sys
 
-print(sys.argv[1])
+from colorama import Style
+from libgentools import SearchRequest
+from libgenx_common import get_query_arg
+
+
+def main():
+    """Main function of the LibGenX CLI script."""
+    print(Style.BRIGHT + "\nWelcome to LibGenX!" + Style.RESET_ALL)
+    query = get_query_arg()
+
+    # main loop
+    while True:
+        if not query:
+            query = input("Enter search query: ")
+        request = SearchRequest(query)
+        print(f"\n{Style.BRIGHT}Search query: {Style.RESET_ALL}{query}")
+        print(f"{Style.BRIGHT}Results: {Style.RESET_ALL}"
+              f"{len(request.results)}")
+
+        while True:
+            c = input("\nFilter results, new search or quit [f/s/q]: ").lower()
+            match c:
+                # filter results
+                case "f":  # TODO filter results
+                    print("filter...")
+                    continue
+                # new search
+                case "s":
+                    query = None
+                    break
+                # close application
+                case "q":
+                    sys.exit(f"\n{Style.BRIGHT}Goodbye!{Style.RESET_ALL}")
+                # invalid command
+                case _:
+                    print("Available commands: [f]ilter, [s]earch, [q]uit!")
+                    continue
+
+
+if __name__ == '__main__':
+    main()
