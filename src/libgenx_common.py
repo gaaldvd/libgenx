f31da498e8328b6d1960534a27cf236a09088274
gaaldvd <gaaldvd@proton.me>
2024-12-16 21:00:57 +0100

    cli: filtering cli arguments added

--- src/libgenx_common.py
+++ src/libgenx_common.py
@@ -5,9 +5,11 @@ LibGenX common module.
 from sys import argv
 from json import load
 
-def get_query_arg():
-    """Parse query from the command line."""
-    return argv[1] if len(argv) > 1 else None
+def get_cli_args():
+    """Parse arguments from the command line."""
+    query = argv[1] if len(argv) > 1 else None
+    seq = argv[2] if len(argv) > 2 else None
+    return query, seq
 
 def load_config():
     """Load configuration from config.json."""
